c8eb5e505ad6ce74a02c56e03dbc4b2009bf5a27
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-07-31 21:58:05 +0700

    init git

---
+++ src/extension.ts
@@ -0,0 +1,41 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import {Position} from 'vscode';
+import { quote } from './randomQuote';
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "Mr.S" is now active!');
+	
+	// the extension will keep track the cursor position. If the position of the cursor doesn't change in 5 minutes, the extension will show a message
+	let lastPosition: Position | undefined = undefined;
+	let timeoutTime = 5 * 60 * 1000; // 5 minutes
+
+	// get the position of the cursor
+	function getPosition(): Position | undefined {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const position = editor.selection.active;
+			if (position) {
+				return position;
+			}
+		}
+		return undefined;
+	}
+
+	setInterval(() => {
+		const position = getPosition();
+		if (position) {
+			if (lastPosition && lastPosition.line === position.line && lastPosition.character === position.character) {
+				vscode.window.showInformationMessage(quote());
+			}
+			lastPosition = position;
+		} 
+	}, timeoutTime);
+	// context.subscriptions.push();
+}
+
---
+++ src/randomQuote.ts
@@ -0,0 +1,11 @@
+const quotes = [
+    'Cỡ thầy hết năm 2 là đã tự học ngôn ngữ lập trình công ty yêu cầu 1 tuần mà các lập trình viên khác tốn 3 tháng',
+    `Don't expect to be easy to work, because if you're 
+    easy to do, your heart will be flattered and arrogant.`,
+    'If you think you are the best programmer then there will be an indigo programmer who is better than you',
+    'Code thủ tục ít thôi ông',
+];
+
+export function quote() {
+    return quotes[Math.floor(Math.random() * quotes.length)];
+}
